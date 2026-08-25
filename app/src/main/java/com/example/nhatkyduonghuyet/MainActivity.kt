9c6409addd4be1f578526cda4439fff59be1f6fb
hqkhanh62 <59009069+hqkhanh62@users.noreply.github.com>
2026-08-25 21:59:14 +0700

    Update MainActivity.kt

--- app/src/main/java/com/example/nhatkyduonghuyet/MainActivity.kt
+++ app/src/main/java/com/example/nhatkyduonghuyet/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.nhatkyduonghuyet
 
+import android.content.Context
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -8,6 +9,7 @@ import androidx.navigation.compose.rememberNavController
 import com.example.nhatkyduonghuyet.ui.navigation.AppNavHost
 import com.example.nhatkyduonghuyet.ml.GlucosePredictor
 import com.example.nhatkyduonghuyet.ml.GlucoseScanner
+import com.example.nhatkyduonghuyet.util.LocaleHelper
 import dagger.hilt.android.AndroidEntryPoint
 import javax.inject.Inject
 
@@ -15,6 +17,11 @@ import javax.inject.Inject
 class MainActivity : ComponentActivity() {
     @Inject lateinit var predictor: GlucosePredictor
     @Inject lateinit var scanner: GlucoseScanner
+
+    override fun attachBaseContext(newBase: Context) {
+        val helper = LocaleHelper(newBase)
+        super.attachBaseContext(helper.applyLocale(newBase))
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
