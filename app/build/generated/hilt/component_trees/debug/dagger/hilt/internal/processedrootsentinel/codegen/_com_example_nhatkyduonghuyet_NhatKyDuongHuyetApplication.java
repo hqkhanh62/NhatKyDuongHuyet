3b998eb833afefc121d70aa08556d281bec08408
HQKhanh <hqkhanh62@gmail.com>
2026-08-27 22:31:25 +0700

    Ignore generated Android build artifacts

--- app/build/generated/hilt/component_trees/debug/dagger/hilt/internal/processedrootsentinel/codegen/_com_example_nhatkyduonghuyet_NhatKyDuongHuyetApplication.java
+++
@@ -1,9 +0,0 @@
-package dagger.hilt.internal.processedrootsentinel.codegen;
-
-import dagger.hilt.internal.processedrootsentinel.ProcessedRootSentinel;
-
-@ProcessedRootSentinel(
-    roots = "com.example.nhatkyduonghuyet.NhatKyDuongHuyetApplication"
-)
-public final class _com_example_nhatkyduonghuyet_NhatKyDuongHuyetApplication {
-}
